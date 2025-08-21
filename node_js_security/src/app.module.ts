44194a565663cfeb2858260bd0d6aeda0322e9b4
Artem Shkonda <artemshkonda@MacBook-Pro.local>
2025-08-21 22:08:52 +0300

    add throrrler

--- node_js_security/src/app.module.ts
+++ node_js_security/src/app.module.ts
@@ -3,17 +3,33 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { AdminModule } from './admin/admin.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
     AdminModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60_000,
+          limit: 10,
+        },
+      ],
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
     }),
     AdminModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}
